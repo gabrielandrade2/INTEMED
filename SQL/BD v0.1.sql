3b85013892ed67092b4514e52ca74a086edcce60
fernando <fernando@fernando-PC.home>
2013-11-26 14:59:40 -0200

    iniciadas as versões do db do zero novamente. A versão zero deve conter o banco de dados inicial, que é o que todos já tem, e esta inclui colunas na tabela resultado, subregra, termo regra e termosubregra.

--- SQL/BD v0.1.sql
+++ SQL/BD v0.1.sql
@@ -4,3 +4,15 @@ ADD COLUMN   `termoAnterior` varchar(45) DEFAULT NULL,
 ADD COLUMN   `palavraAnterior` varchar(45) DEFAULT NULL,
 ADD COLUMN   `termoPosterior` varchar(45) DEFAULT NULL,
 ADD COLUMN   `palavraPosterior` varchar(45) DEFAULT NULL;;
+
+ALTER TABLE `intemed`.`subregras` 
+ADD COLUMN `positivaNegativaNeutra` INT NULL AFTER `texto`;
+
+ALTER TABLE `intemed`.`subregras` 
+ADD COLUMN `distanciaRegra` INT(11) NULL AFTER `positivaNegativaNeutra`;
+
+ALTER TABLE `intemed`.`termosregras` 
+ADD COLUMN `classifMorfolPalavraRaiz` INT(11) NULL AFTER `termo`;
+
+ALTER TABLE `intemed`.`termossubregras` 
+ADD COLUMN `classifMorfolPalavraRaiz` INT(11) NULL AFTER `termo`;
